71fa908616c0d18eef265a5310f464e21b4038a6
KarinaBertosh <cherrycake246@gmail.com>
2022-12-17 20:37:58 +0300

    feat: add build

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '56950494432143cf891079413f679361', // получите свой ключ https://newsapi.org/
         });
     }
